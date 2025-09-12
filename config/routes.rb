d4c5b8c9b1203cf75434c5b4ba1e8b71df92d01d
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-12 20:28:36 +0000

    [Add] topページに一覧画面へのリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destory'
   resources :books
   get 'homes/top', as: 'top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
