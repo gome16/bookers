578a106eaa8e2fd4d0c6978113a30d5e9bbfd05f
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-12 20:03:36 +0000

    [Add] booksコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destory'
   resources :books
   get 'homes/top', as: 'top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
