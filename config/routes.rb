8eb532f383de258b3d37f22916fba7cf329e2e58
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-12 19:03:27 +0000

    [Add] bookersルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookers/index'
-  get 'bookers/show'
-  get 'bookers/create'
-  get 'bookers/edit'
-  get 'bookers/update'
-  get 'bookers/destory'
+  resources :books
   get 'homes/top', as: 'top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
