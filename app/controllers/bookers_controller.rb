d5b37f1425dbd58dc7c4b4753f1f3f9955998499
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-12 18:59:16 +0000

    [Add] bookersコントローラ作成

---
+++ app/controllers/bookers_controller.rb
@@ -0,0 +1,19 @@
+class BookersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookers/index'
+  get 'bookers/show'
+  get 'bookers/create'
+  get 'bookers/edit'
+  get 'bookers/update'
+  get 'bookers/destory'
   get 'homes/top', as: 'top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
