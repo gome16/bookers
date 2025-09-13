11407b3f1576c700c6d795503509ae7a48004b24
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-13 22:26:56 +0000

    [Add] バリテーション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       flash.now[:alert] = "book was not created"
       render :index
     end
@@ -24,10 +25,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully edited."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       flash.now[:alert] = "book was not edited"
       render :edit
